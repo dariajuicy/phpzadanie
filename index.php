0aa20330e2ea351c5ecba48de43b97287295485d
Daria <binxyz01@gmail.com>
2022-11-06 14:21:55 +0100

    add exceptions to deal with errors when occured

--- index.php
+++ index.php
@@ -8,14 +8,29 @@ namespace App;
 // To powinno byc na wersji na swiat. Dla nas bledy sa wazne.
 // error_reporting(0);
 // ini_set('diaplay_errors', '0');
-
+require_once('./Exception/AppException.php');
+require_once('./Exception/StorageException.php');
+require_once('./Exception/ConfigurationExpection.php');
 require_once('./src/Controller.php');
 include_once('.src/utils/debug.php');
 require_once('./config/config.php');
 
-Controller::initConfiguration($configuration);
-$controller = new Controller($_GET, $_POST);
-$controller->run();
+use App\Exception\AppException;
+use App\Exception\StorageException;
+use App\Exception\ConfigurationException;
+use Throwable;
+
+try {
+    Controller::initConfiguration($configuration);
+    $controller = new Controller($_GET, $_POST);
+    $controller->run();
+} catch (AppException $e) {
+    echo "<h1>Wystąpił błąd w aplikacji</h1>";
+    echo '<h3>' . $e->getMessage() . '</h3>';
+} catch (Throwable $e) {
+    echo "<h1>Wystąpił błąd w aplikacji</h1>";
+}
+
 $viewParams = [];
 
 switch ($action) {
--- src/database.php
+++ src/database.php
@@ -4,18 +4,36 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Exception\StorageException;
+use App\Exception\ConfigurationException;
+
 use PDO;
+use PDOException;
+use Throwable;
+
 
 class Database
 {
     public function __construct(array $config)
     {
-        $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
-        //$connection = new PDO(
-        //    $dsn,
-        //    $config['user'],
-        //    $config['password'],
-        // );
-        $connection = new PDO('sss');
+       try {
+        $this->validateConfig($config);
+        $dsn = "mysql:dbname=[$config['database']};host={$config['host']}";
+        $connection = new PDO(
+            $dsn,
+            $config['user'];
+            $config['password'];
+        );
+       } catch (PDOException $e) {
+        throw new StorageException('Connection error');
+       }
     }
-}
+
+    private function validateConfig(array $config): void
+
+ {
+    if (empty($config['database']) || empty ($config['user']) || empty($config['host'])) {
+        throw new ConfigurationException('Problem z konfiguracją - skontaktuj się z administratorem');
+        
+    }
+ }
